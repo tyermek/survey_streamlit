4152bc1196b744dfa1c8521ef3b60c535fdd088b
tyermek <129354136+tyermek@users.noreply.github.com>
2024-07-13 23:28:23 +0500

    Add files via upload

---
+++ pages/1_Сауалнаманы өңдеу.py
@@ -0,0 +1,136 @@
+import time
+import streamlit as st
+import json
+import os
+
+# Set page configuration
+st.set_page_config(page_title="Сауалнаманы өңдеу", page_icon="📊")
+st.sidebar.header("Сауалнаманы өңдеу")
+st.markdown("# Сауалнаманы өңдеу")
+
+# Check if the user is logged in
+if not st.session_state.get("password_correct", False):
+    st.error("Алдымен жүйеге кіру керек!")
+    st.stop()
+
+# File paths
+QUESTIONS_FILE = "C:/Users/toleb/OneDrive/Рабочий стол/Tele2/Python_scripts/pythonProject/questions.json"
+
+
+# Load questions
+def load_questions(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return []
+
+
+# Save questions
+def save_questions(questions, file_path):
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(questions, f, ensure_ascii=False, indent=4)
+
+
+# Load existing questions
+questions_with_options = load_questions(QUESTIONS_FILE)
+
+
+# Function to add a new question
+def add_question():
+    new_question = {
+        "question": st.session_state['question_text'],
+        "options": st.session_state['answer_options'],
+        "type": "RADIO" if st.session_state['question_type'] == "Бір жауабы бар" else "CHECKBOX"
+    }
+    questions_with_options.append(new_question)
+    save_questions(questions_with_options, QUESTIONS_FILE)
+    st.success("Сұрақ қосылды!")
+    time.sleep(2)
+    reset_form()
+
+
+def reset_form():
+    st.session_state.clear()
+    st.experimental_rerun()
+
+
+# Initialize session state for answer options and question text
+if 'answer_options' not in st.session_state:
+    st.session_state['answer_options'] = []
+
+if 'question_text' not in st.session_state:
+    st.session_state['question_text'] = ""
+
+if 'new_option' not in st.session_state:
+    st.session_state['new_option'] = ""
+
+if 'question_type' not in st.session_state:
+    st.session_state['question_type'] = "Бір жауабы бар"
+
+if 'show_questions' not in st.session_state:
+    st.session_state['show_questions'] = False
+
+
+# Function to add an option
+def add_option():
+    if st.session_state['new_option']:
+        st.session_state['answer_options'].append(st.session_state['new_option'])
+        st.session_state['new_option'] = ""  # Clear the input after adding
+
+
+# Function to show all questions
+def show_all_questions():
+    st.session_state['show_questions'] = True
+    st.experimental_rerun()
+
+
+# Streamlit form for adding questions
+if not st.session_state['show_questions']:
+    st.subheader("Жаңа сұрақ қосу")
+    st.selectbox("Сұрақ түрін таңдаңыз", options=["Бір жауабы бар", "Бір немесе бірнеше дұрыс жауабы бар"],
+                 key='question_type')
+    st.text_input("Сұрақты енгізіңіз", key='question_text')
+
+    # Section to add options one by one
+    st.text_input("Жауап нұсқасын енгізіңіз", key='new_option')
+    st.button("Жауап нұсқасын қосу", on_click=add_option)
+
+    # Display the current question and added options
+    if st.session_state['question_text']:
+        st.write("Қосылған сұрақ:")
+        st.write(st.session_state['question_text'])
+
+    if st.session_state['answer_options']:
+        st.write("Қосылған жауап нұсқалары:")
+        for idx, option in enumerate(st.session_state['answer_options']):
+            st.write(f"{idx + 1}. {option}")
+
+    # Use narrower columns to place buttons closer
+    add_question_html = st.button("Сұрақты дерекқорға қосу", key="add_question_html")
+    show_questions_html = st.button("Дерекқорды көрсету", key="show_questions_html")
+
+    if add_question_html:
+        if not st.session_state['question_text']:
+            st.error("Сұрақты енгізіңіз")
+        elif len(st.session_state['answer_options']) < 2:
+            st.error("Кем дегенде екі жауап нұсқасын енгізіңіз")
+        else:
+            add_question()
+
+    if show_questions_html:
+        show_all_questions()
+
+# Display all questions
+else:
+    st.subheader("Барлық сұрақтар")
+    for q in questions_with_options:
+        st.write(f"Сұрақ: {q['question']}")
+        for idx, option in enumerate(q['options']):
+            st.write(f"{idx + 1}. {option}")
+        st.write(f"Түрі: {'Бір жауабы бар' if q['type'] == 'RADIO' else 'Бір немесе бірнеше дұрыс жауабы бар'}")
+        st.write("---")
+
+    # Button to go back to the form
+    if st.button("Жаңа сұрақ қосу"):
+        st.session_state['show_questions'] = False
+        st.experimental_rerun()
---
+++ pages/3_Анализ.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Set page configuration
+st.set_page_config(page_title="Деректерді талдау", page_icon="📊")
+st.sidebar.header("Деректерді талдау")
+st.markdown("# Деректерді талдау")
+
+# Check if the user is logged in
+if not st.session_state.get("password_correct", False):
+    st.error("Алдымен жүйеге кіру керек!")
+    st.stop()
+
+
+# Function to load the Excel file with caching
+@st.cache_data
+def load_data():
+    return pd.read_excel("C:/Users/toleb/OneDrive/Рабочий стол/Tele2/Python_scripts/pythonProject/35950.xlsx",
+                         sheet_name="35950")
+
+
+# Load the data
+df = load_data()
+
+# Remove everything after "/" in the responses
+for col in df.columns:
+    df[col] = df[col].astype(str).str.split('/ ').str[0].str.strip()
+
+# Define aliases for specific columns
+question_aliases = {
+    "Өзіңіздің аймағыңызды таңдаңыз:": "Аймақ",
+    "Сіз қандай мектепте оқисыз?": "Мектеп",
+    "4. Мектептегі оқыту тілін белгілеңіз / Укажите язык обучения в школе:": "Оқыту тілі",
+    "Өз статусыңызды көрсетіңіз: ": "Статус"
+}
+
+# Display filter options for the specified questions using their aliases
+filters = {}
+for question, alias in question_aliases.items():
+    if question in df.columns:
+        unique_values = df[question].unique().tolist()
+        filters[question] = st.sidebar.multiselect(f"Сүзгі: {alias}", options=unique_values)
+
+# Apply filters
+filtered_df = df.copy()
+for question, selected_values in filters.items():
+    if selected_values:
+        filtered_df = filtered_df[filtered_df[question].isin(selected_values)]
+
+
+# Plot overall statistics
+def plot_overall_stats(df):
+    st.subheader("Статистика")
+    col1, col2 = st.columns(2)
+    col1.metric("Жауаптар саны", len(df))
+    col2.metric("Сұрақтар саны", len(df.columns) - 2)  # Subtract 2 for Response ID and Submitted at
+
+
+# Plot responses
+def plot_responses(df):
+    # Availability of Workspace
+    st.markdown("### Үйде жұмыс орнының қолжетімділігі")
+    fig = px.pie(df,
+                 names="Қашықтықтан оқыту кезінде сабақтарды орындау үшін Сіздің үйде жұмыс орны бар ма? ",
+                 title="Cабақтарды орындау үшін Сіздің үйде жұмыс орны бар ма?")
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+    fig = px.bar(df,
+                 x="16.	Компьютерде күніне қанша сағат отырасыз? / Сколько часов в день Вы сидите за компьютером?",
+                 title="Компьютерде күніне қанша сағат отырасыз?")
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+    fig = px.bar(df,
+                 x="15.	Үй жағдайында Сізге онлайн сабақтарға қатысу ыңғайлы ма? / Удобно ли Вам в домашних условиях участвовать в онлайн уроках? ",
+                 title="Сізге онлайн сабақтарға қатысу ыңғайлы ма?")
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+    fig = px.pie(df,
+                 names="27.	Компьютер немесе гаджет салдарынан отбасы мүшелерімен жанжал туындайды ма? / Возникают ли ссоры с членами семьи из-за компьютера или гаджета?",
+                 title="Компьютер немесе гаджет салдарынан отбасы мүшелерімен жанжал туындайды ма?")
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+    # Experience with Remote Learning
+    st.markdown("### Қашықтықтан оқытудың өту тәжірибесі")
+    fig = px.pie(df,
+                 names="10.	Мұғалімнің тапсырмаларын қалай жиі орындайсыз? / Как часто Вы выполняете задания учителя?",
+                 title="Мұғалімнің тапсырмаларын қалай жиі орындайсыз?")
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+    fig = px.bar(df,
+                 x="13.	Сіз қалай ойлайсыз, сіз қашықтықтан оқыту кезінде белсенді бола алдыңыз ба? / Как вы считате, Вы стали активнее при дистанционном обучении?",
+                 title="Сіз қашықтықтан оқыту кезінде белсенді бола алдыңыз ба?")
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+
+# Display statistics and visualizations
+plot_overall_stats(filtered_df)
+if not filtered_df.empty:
+    plot_responses(filtered_df)
+else:
+    st.warning("Фильтрлерге сәйкес келетін жауаптар жоқ.")
