558e5a7ae187a89d39a6cb5a50aa89ccb89a8076
tyermek <129354136+tyermek@users.noreply.github.com>
2024-07-13 23:42:11 +0500

    Update 2_Визуализация.py

--- pages/2_Визуализация.py
+++ pages/2_Визуализация.py
@@ -14,8 +14,8 @@ st.markdown("# Визуализация")
 # Function to create URL for Google Form
 SCOPES = ["https://www.googleapis.com/auth/forms.body", "https://www.googleapis.com/auth/forms.responses.readonly"]
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
-CREDENTIALS_FILE = "C:/Users/toleb/OneDrive/Рабочий стол/Tele2/Python_scripts/pythonProject/client_secrets.json"
-TOKEN_FILE = "C:/Users/toleb/OneDrive/Рабочий стол/Tele2/Python_scripts/pythonProject/token.pickle"
+CREDENTIALS_FILE = "../client_secrets.json"
+TOKEN_FILE = "../token.pickle"
 
 
 def get_credentials():
